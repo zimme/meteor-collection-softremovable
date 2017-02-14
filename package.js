53031e8e9b2d07232c82b3b05a825217e41e740e
Simon Fridlund <simon@fridlund.email>
2017-02-14 18:00:53 +0100

    Bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-collection-softremovable.git',
   name: 'zimme:collection-softremovable',
   summary: 'Add soft remove to collections',
-  version: '1.0.6-alpha.3'
+  version: '1.0.6-beta.1'
 });
 
 Package.onUse(function(api) {
